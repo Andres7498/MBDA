033907ce574f78e6274967a5e441742416d1cab1
Andres7498 <rixco1998@hotmail.com>
2020-05-20 12:49:24 -0500

    proyecto

--- Proyecto/proyecto.sql
+++ Proyecto/proyecto.sql
@@ -255,5 +255,17 @@ ALTER TABLE Boletas ADD CONSTRAINT FK_Boletas_Funciones FOREIGN KEY (idFuncion) 
 /*VISTAS*/
 CREATE VIEW V_Boleta AS
 	SELECT * FROM Boletas JOIN Ubicaciones ON (Boletas.idUbicacion=Ubicaciones.idUbicacion);
-    
-    
+CREATE VIEW V_Peliculas AS
+	SELECT idPelicula, idGenero, nombre, director, duracion, horaInicio, horaFin, clasificacion
+	FROM Funciones JOIN Peliculas ON (idPelicula=idPelicula) JOIN Generos ON (idGenero=idGenero);
+CREATE VIEW V_Clientes AS
+	SELECT idCliente, primerNombre,segundoNombre, apellidos, documento, puntajePelicula, calificacionServicios, generoFavorito
+	FROM Clientes JOIN EncuestasDeSatisfaccion ON (idCliente=idCliente);
+/*ELIMINAR VISTAS*/
+DROP VIEW V_Boletas;
+DROP VIEW V_Peliculas;
+DROP VIEW v_Clientes;
+
+/*ELIMINAR TABLAS*/
+
+DROP TABLE ACTIVIDAD;
